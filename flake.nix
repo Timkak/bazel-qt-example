da771628c806bbf4cc3c1d780655e63507079d91
Tim Anishchuk <timanishchuk@gmail.com>
2025-04-05 14:04:41 +0300

    delete graphics

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "A very basic flake";
+  description = "Qt development environment with Bazel";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
@@ -10,51 +10,48 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     base = pkgs.appimageTools.defaultFhsEnvArgs;
-    fhs = pkgs.buildFHSEnv (base //  {
-      name = "QT environment";  
+    fhs = pkgs.buildFHSEnv (base // {
+      name = "QT";
       targetPkgs = pkgs: with pkgs; [
         bazel_7
         cmake
         gcc13
-        llvmPackages_18.clang-tools
-        llvmPackages_18.clang
-	libxkbcommon
+        clang
+        binutils
+        qt6.full
         libGL
-        libGLU
-        xorg.libX11
-        xorg.libXext
-        xorg.libXrender
-        xorg.libXi
-        xorg.libXrandr
-        xorg.libXfixes
-        xorg.libXcursor
-        xorg.libxcb
-        xorg.xcbutil
-        xorg.xcbutilkeysyms
-        xorg.xcbutilwm
-        xorg.xcbutilimage
-        # Multimedia and system dependencies
-        alsa-lib
-        libpulseaudio
-        dbus
-        fontconfig
-        freetype
-        openssl
-        # Additional Qt dependencies
-        vulkan-headers
-        vulkan-loader
-        wayland
-        wayland-protocols
+        libglvnd
+        mesa
+        clang-tools_18
+	      clang
       ];
-      extraBwrapArgs = [ "--bind /tmp /tmp" ];
+      
+      extraBuildCommands = ''
+        mkdir -p usr/bin
+        ln -s ${pkgs.gcc13}/bin/gcc usr/bin/gcc
+        ln -s ${pkgs.gcc13}/bin/g++ usr/bin/g++
+        ln -s ${pkgs.binutils}/bin/ld usr/bin/ld
+        
+        # Add OpenGL headers to expected paths
+        mkdir -p usr/include
+        ln -s ${pkgs.libGL.dev}/include/GL usr/include/GL
+      '';
+      
       profile = ''
-        export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt6.qtbase.bin}/lib/qt-6/plugins/platforms"
-        export QT_PLUGIN_PATH="${pkgs.qt6.qtbase.bin}/lib/qt-6/plugins"
-        export QML2_IMPORT_PATH="${pkgs.qt6.qtbase.bin}/lib/qt-6/qml"
+        export CC=${pkgs.gcc13}/bin/gcc
+        export CXX=${pkgs.gcc13}/bin/g++
+        export PATH=${pkgs.gcc13}/bin:${pkgs.binutils}/bin:$PATH
+        export QT_PATH=${pkgs.qt6.full}
+        export QT_PLUGIN_PATH="$QT_PATH/${pkgs.qt6.qtbase.qtPluginPrefix}"
+        export QML2_IMPORT_PATH="$QT_PATH/${pkgs.qt6.qtbase.qtQmlPrefix}"
+        
+        # Add OpenGL library paths
+        export LD_LIBRARY_PATH=${pkgs.libGL}/lib:${pkgs.libglvnd}/lib:${pkgs.mesa}/lib:$LD_LIBRARY_PATH
+        export C_INCLUDE_PATH=${pkgs.libGL.dev}/include:$C_INCLUDE_PATH
+        export CPLUS_INCLUDE_PATH=${pkgs.libGL.dev}/include:$CPLUS_INCLUDE_PATH
       '';
     });
   in {
     devShells.${system}.default = fhs.env;
   };
 }
-
